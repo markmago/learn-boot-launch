1443bf5bc554f3c819e343a66a758270b4489975
hustmark <hustmark@hust.edu.cn>
2020-02-09 20:46:17 +0800

    P15 获取自定义配置文件ççå值得两种方法

---
+++ src/main/java/com/markma/learnbootlaunch/model/yaml/Father.java
@@ -0,0 +1,13 @@
+package com.markma.learnbootlaunch.model.yaml;
+
+import lombok.Data;
+
+/*
+@author mark
+@time 2020/2/9
+*/
+@Data
+public class Father {
+    private String name;
+    private Integer age;
+}
