82658a93b93dbcebb03f1f64b491c164823979f7
hustmark <hustmark@hust.edu.cn>
2020-02-09 15:25:19 +0800

    P12 使用swagger2构建发布api文档

--- src/main/java/com/markma/learnbootlaunch/Controller/ArticleRestController.java
+++ src/main/java/com/markma/learnbootlaunch/Controller/ArticleRestController.java
@@ -2,6 +2,7 @@ package com.markma.learnbootlaunch.Controller;
 
 import com.markma.learnbootlaunch.model.AjaxResponse;
 import com.markma.learnbootlaunch.model.Article;
+import io.swagger.annotations.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,10 +11,15 @@ import java.util.Date;
 @Slf4j
 @RestController
 @RequestMapping("/rest")
+@Api(tags = "Article")
 public class ArticleRestController {
  
     //增加一篇Article ，使用POST方法
     //@RequestMapping(value = "/article", method = RequestMethod.POST, produces = "application/json")
+    @ApiOperation(value = "添加文章", notes = "添加新的文章",httpMethod = "POST")
+//    @ApiResponses({
+//            @ApiResponse(code=200,message="成功",response=AjaxResponse.class),
+//    })
     @PostMapping("/article")
     public AjaxResponse saveArticle(@RequestBody Article article) {
         //因为使用了lombok的Slf4j注解，这里可以直接使用log变量打印日志
@@ -35,6 +41,7 @@ public class ArticleRestController {
     //删除一篇Article，使用DELETE方法，参数是id
    // @RequestMapping(value = "/article/{id}", method =RequestMethod.DELETE, produces = "application/json")
     @DeleteMapping("/article/{id}")
+    @ApiOperation(value = "删除文章", notes = "删除某篇文章", httpMethod = "DELETE")
     public AjaxResponse deleteArticle(@PathVariable Long id) {
         log.info("deleteArticle：{}",id);
         return AjaxResponse.success(id);
@@ -43,6 +50,7 @@ public class ArticleRestController {
      //更新一篇Article，使用PUT方法，以id为主键进行更新
     //@RequestMapping(value = "/article/{id}", method = RequestMethod.PUT, produces = "application/json")
     @PutMapping("/article/{id}")
+    @ApiOperation(value = "更新文章", notes = "更新文章",httpMethod = "PUT")
     public AjaxResponse updateArticle(@PathVariable Long id, @RequestBody Article article) {
         article.setId(id);
         log.info("updateArticle：{}",article);
@@ -52,6 +60,7 @@ public class ArticleRestController {
     //获取一篇Article，使用GET方法
     //@RequestMapping(value = "/article/{id}", method = RequestMethod.GET, produces = "application/json")
     @GetMapping("/article/{id}")
+    @ApiOperation(value = "获取文章", notes = "获取某篇文章", httpMethod = "GET")
     public AjaxResponse getArticle(@PathVariable Long id) {
 
         //使用lombok提供的builder构建对象
--- src/main/java/com/markma/learnbootlaunch/model/Article.java
+++ src/main/java/com/markma/learnbootlaunch/model/Article.java
@@ -2,6 +2,8 @@ package com.markma.learnbootlaunch.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,11 +20,16 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @Builder
+@ApiModel("文章")
 public class Article {
     @JsonIgnore
+    @ApiModelProperty("id编号")
     private Long  id;
+    @ApiModelProperty("作者")
     private String author;
+    @ApiModelProperty("标题")
     private String title;
+    @ApiModelProperty("文章内容")
     private String content;
     //@JsonFormat(pattern = "yyyy-MM-dd hh:mm:ss",timezone = "GMT+8")
     private Date createTime;
